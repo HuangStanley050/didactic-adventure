81a0bedbb46f392ec55604a50a3e03e56385f6ed
Stanley Huang <huang.stanley050@gmail.com>
2020-02-27 17:00:40 +1100

    made anime data available for later use

---
+++ src/convert.js
@@ -0,0 +1,16 @@
+const rawData = require("./animeDataRaw.json");
+const fs = require("fs");
+
+const data = JSON.parse(JSON.stringify(rawData));
+
+const animeList = data.urlset.url;
+let finalData = {};
+
+animeList.forEach(anime => {
+  let temp = anime.loc.split("/");
+  //console.log(temp[temp.length - 2], temp[temp.length - 1]);
+  let result = temp[temp.length - 1].replace(/_/g, " ");
+  finalData[temp[temp.length - 2]] = result;
+});
+
+fs.writeFileSync("animeData.json", JSON.stringify(finalData));
