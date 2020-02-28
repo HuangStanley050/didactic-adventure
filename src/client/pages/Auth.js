266c7fa7716741df8781e720edc10e75d92e55b3
Stanley Huang <huang.stanley050@gmail.com>
2020-02-28 12:53:57 +1100

    login page ui done

--- src/client/pages/Auth.js
+++ src/client/pages/Auth.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Container, Row, Col, Button } from "reactstrap";
 
 const Auth = () => {
-  return <h1>Auth page</h1>;
+  const colStyle = {
+    display: "flex",
+    justifyContent: "center",
+    margin: "2rem auto"
+  };
+
+  return (
+    <div style={{ marginTop: "5rem" }}>
+      <h2 className="text-primary" style={{ textAlign: "center" }}>
+        Login Method
+      </h2>
+      <Container
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+        }}
+      >
+        <Row style={{ width: "80%" }}>
+          <Col style={colStyle} xs="12">
+            <Button style={{ display: "block", width: "60%" }} color="primary">
+              Google
+            </Button>
+          </Col>
+          <Col style={colStyle} xs="12">
+            <Button style={{ display: "block", width: "60%" }} color="info">
+              Twitter
+            </Button>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 };
 
 export default {
