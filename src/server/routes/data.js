cfcf9540a89696c7c7a054e950ff86f962476432
Stanley Huang <huang.stanley050@gmail.com>
2020-03-07 18:10:14 +1100

    get a random anime id from animeData object

--- src/server/routes/data.js
+++ src/server/routes/data.js
@@ -5,9 +5,23 @@ const router = express.Router();
 router.get("/api/anime", (req, res) => {
   const animes = Object.keys(animeData).length;
   const selection = Math.floor(Math.random() * animes) + 1;
-  //console.log(Object.keys(animeData).length);
-  console.log("randomly selected anime is: ", animeData[selection.toString()]);
-  console.log("id is: ", selection);
+  const animeArray = Object.entries(animeData);
+  let randomAnimeId = 1;
+
+  for (const [id, title] of animeArray) {
+    if (id === selection.toString()) {
+      console.log("selection is: ", selection);
+      console.log("id is: ", id);
+      randomAnimeId = id;
+      break;
+    }
+  }
+  console.log("random anime id: ", randomAnimeId);
+  console.log(
+    "randomly selected anime is: ",
+    animeData[randomAnimeId.toString()]
+  );
+  console.log("id is: ", randomAnimeId);
   res.send("anime route");
 });
 
