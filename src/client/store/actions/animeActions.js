d1d98f088645e08690fdeffadfccfd8cad27e947
Stanley Huang <huang.stanley050@gmail.com>
2020-03-10 20:38:00 +1100

    front end connected with back end /api/anime

--- src/client/store/actions/animeActions.js
+++ src/client/store/actions/animeActions.js
@@ -1,31 +1,21 @@
 import ActionType from "./index";
 
-const animeId = [537, 30, 123];
-const dataApi = `https://api.jikan.moe/v3/anime/`;
+//const animeId = [1, 123, 30];
+const dataApi = `/api/anime`;
 export const fetchDefaultAnimes = () => {
   return async (dispatch, getState, api) => {
     dispatch({ type: ActionType.FETCH_DEFAULT_ANIMES_START });
-    const animePromise1 = api.get(dataApi + animeId[0]);
-    const animePromise2 = api.get(dataApi + animeId[1]);
-    //const animePrimise3 = api.get(dataApi + animeId[2]);
-    let result = await Promise.all([
-      animePromise1,
-      animePromise2
-      //animePrimise3
-    ]);
-    let animeData = result.map(anime => {
-      // console.log(anime.data.title);
-      // console.log(anime.data.type);
-      // console.log(anime.data.synopsis);
-      // console.log(anime.data.image_url);
+    let result = await api.get(dataApi);
+    //console.log(result.data);
+    let animeData = result.data.data.map(anime => {
       return {
-        title: anime.data.title,
-        type: anime.data.type,
-        summary: anime.data.synopsis,
-        imgURL: anime.data.image_url
+        title: anime.title,
+        type: anime.type,
+        summary: anime.synopsis,
+        imgURL: anime.image_url
       };
     });
-    //console.log(animeData);
+
     dispatch({ type: ActionType.FETCH_DEFAULT_ANIMES, payload: animeData });
   };
 };
